08da2b61cb6ab7c5e421745a60fb32d60e505ce8
Ali Momen Sani <ali.momensany@gmail.com>
2018-03-19 19:59:20 +0330

    add file to test

---
+++ send_file_to_telegram.py
@@ -0,0 +1,34 @@
+import os
+from datetime import datetime
+
+from telegram.ext import Updater
+
+import ffmpeg_util
+import utils
+
+
+def send_to_telegram(file_name):
+    print('{}: sending {} to telegram channel...'.format(utils.beautiful_now(), file_name))
+    updater = Updater("473515704:AAFvU-mxPNHOD98iagHdfaCPAeOAduIw-1M")
+
+    scaled_path = '{}-{}-scaled.mp4'.format(file_name, int(datetime.now().timestamp() * 1e3))
+    scaled = ffmpeg_util.scale_video_good(file_name, scaled_path, 360)
+
+    if scaled:
+        print('{}: scaled {} to {}'
+              .format(utils.beautiful_now(), file_name, os.path.getsize(file_name), os.path.getsize(scaled_path)))
+        # os.remove(file_name)
+        file_name = scaled_path
+
+    print('{}: sending {} sized {} to telegram channel...'
+          .format(utils.beautiful_now(), file_name, os.path.getsize(file_name)))
+
+    with open(file_name, 'rb') as fo:
+        try:
+            updater.bot.send_video(chat_id='@GifsSubreddit', video=fo,
+                                   caption='test 🤓',
+                                   timeout=60)
+            print('{}: uploaded {} to telegram.'.format(utils.beautiful_now(), file_name))
+        except Exception as e:
+            print('{} {}'.format(file_name, e))
+            pass
